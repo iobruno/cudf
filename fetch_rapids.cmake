6bc5ac1d9aedf2661ca4b0b4f8934e5c6a8f3759
Yunsong Wang <yunsongw@nvidia.com>
2024-01-23 12:10:11 -0800

    Fetch custom rapids cmake

--- fetch_rapids.cmake
+++ fetch_rapids.cmake
@@ -1,5 +1,5 @@
 # =============================================================================
-# Copyright (c) 2018-2023, NVIDIA CORPORATION.
+# Copyright (c) 2018-2024, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
@@ -12,6 +12,8 @@
 # the License.
 # =============================================================================
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake)
+  set(rapids-cmake-repo PointKernel/rapids-cmake)
+  set(rapids-cmake-branch cuco-experimental)
   file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-24.04/RAPIDS.cmake
        ${CMAKE_CURRENT_BINARY_DIR}/CUDF_RAPIDS.cmake
   )
